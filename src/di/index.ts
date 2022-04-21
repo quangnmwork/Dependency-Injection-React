05e8e5f0481d97e822ec354823ed5bd79a5340e8
Quang Nguyen <quangnm.work@gmail.com>
2022-04-22 00:41:00 +0700

    adding injector

--- src/di/index.ts
+++ src/di/index.ts
@@ -12,3 +12,39 @@ export function createBinding<T>(
 ): IBinding<T> {
   return { token, provider };
 }
+
+export default class Injector {
+  public bindings = new Map<IToken<any>, IProvider<any>>();
+  public cache = new Map<IToken<any>, Promise<any>>();
+
+  constructor(binding: IBinding[]) {
+    this.addBingdings(binding);
+  }
+  public addBingdings(bindings: IBinding[]) {
+    for (const binding of bindings) {
+      this.addBingding(binding);
+    }
+  }
+  public addBingding(binding: IBinding) {
+    this.bindings.set(binding.token, binding.provider);
+  }
+  public async get<T>(token: IToken<T>): Promise<T> {
+    if (this.cache.has(token)) {
+      return this.cache.get(token);
+    } else {
+      const promise = this.provide(token);
+      this.cache.set(token, promise);
+      return promise;
+    }
+  }
+  private async provide<T>(token: IToken<T>) {
+    const provider = this.bindings.get(token);
+
+    if (!provider) {
+      throw new Error(`Provider undefined ${token}`);
+    }
+    const dependencies = await Promise.all(
+      provider.dependencyToken.map((t) => this.get(t))
+    );
+  }
+}
